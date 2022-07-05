1a0d50e91a7058e09be8549aedddd2ac59f9aa04
rosercoder <rosercoder@gmail>
2022-07-05 12:12:06 +0800

    feat: Update MainApp.java

--- src/main/java/cool/wangshuo/template/MainApp.java
+++ src/main/java/cool/wangshuo/template/MainApp.java
@@ -1,15 +1,23 @@
 package cool.wangshuo.template;
 
+import cool.wangshuo.template.view.OverviewController;
+import cool.wangshuo.template.view.RootLayoutController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+
 
     /**
      * Constructor
@@ -21,19 +29,66 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Template For JavaFx");
+
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResource("icon/address_book_32.png").toString()));
+
+        initRootLayout();
+
+        showOverview();
+    }
+
+    /**
+     * Initializes the root layout
+     */
+    public void initRootLayout() {
         try {
-            this.rootLayout = new BorderPane();
-            Scene scene = new Scene(this.rootLayout,400,400);
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
+
+            // Give the controller access to the main app.
+            RootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+
             primaryStage.show();
-        } catch(Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Shows the overview inside the root layout.
+     */
+    public void showOverview() {
+        try {
+            // Load  overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("view/Overview.fxml"));
+            AnchorPane overview = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(overview);
+
+            OverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return this.primaryStage;
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 
 }
